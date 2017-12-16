61ec3d484b39c0310f46feceb62f8894b68fae3b
Ralf Kotulla <ralf.kotulla@gmail.com>
2017-12-16 14:28:10 -0600

    added a function to query MPC with timeout.

--- bulk_query.py
+++ bulk_query.py
@@ -3,6 +3,49 @@
 import os, sys, numpy
 
 import podi_ephemerides as ephem
+import multiprocessing
+
+
+def handler(queue, **kwargs):
+
+    # print kwargs
+
+    ephemdata = None
+    # print "querying MPC"
+
+    ephemdata = ephem.get_ephemerides_for_object(**kwargs)
+    #     object_name=object_name,
+    #     start_datetime=start_datetime,
+    #     end_datetime=end_datetime,
+    #     time_interval=time_interval,
+    #     session_log_file=log_file,
+    #     verbose=False,
+    # )
+
+    queue.put(ephemdata)
+
+    return
+
+def query_horizons(timeout, **kwargs):
+
+    queue = multiprocessing.Queue()
+
+    kwargs['queue'] = queue
+    #kwargs['verbose'] = True
+
+    p = multiprocessing.Process(target=handler, kwargs=kwargs)
+    p.start()
+    p.join(timeout)
+    if (p.is_alive()):
+        print "Terminating task after timeout"
+        p.terminate()
+        results = None
+    else:
+        results = queue.get()
+
+    # print kwargs
+
+    return results
 
 if __name__ == "__main__":
 
@@ -20,33 +63,52 @@ if __name__ == "__main__":
         output_dir = "."
         pass
 
+    timeout = 60
 
-    for line in comets[211:]:
+    for line_number, line in enumerate(comets):
+        if (line_number < 583):
+            continue
+
         name = line[102:158]
         # print name
 
         object_name = " ".join(name.split(' ')[:2])
+        if (object_name.find('/') >= 0):
+            object_name = object_name.split("/")[0]
+            print "REFINED:", object_name
 
         save_filename = name.strip()
         for c in [' ', '/', '(', ')', ' ']:
             save_filename = save_filename.replace(c, '_')
-        print "%50s ==> %s" %(name, save_filename)
+        print "% 4d :: %50s ==> %s" %(line_number, name, save_filename)
 
         if (save_filename[-1] == "_"): save_filename = save_filename[:-1]
         log_file = "%s/%s.log" % (output_dir, save_filename)
         ephem_file = "%s/%s.ephem" % (output_dir, save_filename)
 
-        ephemdata = ephem.get_ephemerides_for_object(
-            object_name = object_name,
-            start_datetime = start_datetime,
-            end_datetime = end_datetime,
-            time_interval = time_interval,
-            session_log_file=log_file,
-            verbose=False,
-            )
+        ephemdata = query_horizons(timeout=timeout,
+                                   object_name = object_name,
+                                   start_datetime = start_datetime,
+                                   end_datetime = end_datetime,
+                                   time_interval = time_interval,
+                                   session_log_file=log_file,
+                                   verbose=False,
+                                   )
 
-        numpy.savetxt(ephem_file, ephemdata['data'])
+        # ephemdata = ephem.get_ephemerides_for_object(
+        #     object_name = object_name,
+        #     start_datetime = start_datetime,
+        #     end_datetime = end_datetime,
+        #     time_interval = time_interval,
+        #     session_log_file=log_file,
+        #     verbose=False,
+        #     )
+
+        if (ephemdata is not None):
+            numpy.savetxt(ephem_file, ephemdata['data'])
         #with open(save_filename, "w") as ef:
         #    ef.write(ephemdata)
 
+
+        # break
             
